40f93cb725c47bd13c4e0cb664f1e085ebc588d8
Arthur <arthur001123@gmail.com>
2021-06-06 12:28:21 -0300

    converting jpg image to gray scale png image

--- main.go
+++ main.go
@@ -1,6 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"image/png"
+	"log"
+	"os"
+	"time"
+
+	"github.com/ArthurPaivaT/mergi/convert"
+)
+
 func main() {
+
+	imgFile, err := os.Open("./imageFiles/inputPic.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer imgFile.Close()
+
+	img, err := decode(imgFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	newFile, err := os.Create("./imageFiles/result.png")
+
+	start := time.Now()
+	grayImg := convert.ToGray(img)
+
+	elapsed := time.Since(start)
+	png.Encode(newFile, grayImg)
+	fmt.Println("Took ", elapsed)
 
 	return
 }
