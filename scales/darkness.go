cd2dc2f127a3347b112b2396ff4defebada78ffe
Arthur <arthur001123@gmail.com>
2021-06-06 12:25:42 -0300

    add func to convert get image darkness rate

---
+++ scales/darkness.go
@@ -0,0 +1,31 @@
+package scales
+
+import (
+	"image"
+	"image/color"
+)
+
+func Darkness(img image.Image) float64 {
+	totalPixels := Width(img) * Height(img)
+
+	darkPixels := 0
+	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+			c := color.GrayModel.Convert(img.At(x, y)).(color.Gray)
+
+			if c.Y >= 100 {
+				darkPixels++
+			}
+		}
+	}
+	darkness := float64(darkPixels) / float64(totalPixels)
+
+	return darkness
+}
+
+func Width(i image.Image) int {
+	return i.Bounds().Max.X - i.Bounds().Min.X
+}
+func Height(i image.Image) int {
+	return i.Bounds().Max.Y - i.Bounds().Min.Y
+}
