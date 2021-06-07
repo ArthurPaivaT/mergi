ed63ecdb39535ad6556c0eacb0713556e6871a7c
Arthur <arthur001123@gmail.com>
2021-06-07 12:19:17 -0300

    fix drawing effect round func

--- convert/drawing.go
+++ convert/drawing.go
@@ -25,46 +25,48 @@ func ToDrawing(img image.Image) image.Image {
 func round(c *color.NRGBA) {
 
 	rR := c.R % 51
-
-	if rR > 51 || c.R < 51 {
-		if c.R >= 204 {
+	if rR > 25 {
+		if (c.R + 51 - rR) > 255 {
 			c.R = 255
+		} else {
+			c.R += 51 - rR
 		}
-		c.R += rR
 	} else {
-		if c.R <= 51 {
+		if (c.R - rR) < 0 {
 			c.R = 0
+		} else {
+			c.R -= rR
 		}
-		c.R -= rR
 	}
 
 	rG := c.G % 51
-
-	if (rG > 51) || c.G < 51 {
-		if c.G >= 204 {
+	if rG > 25 {
+		if (c.G + 51 - rG) > 255 {
 			c.G = 255
+		} else {
+			c.G += 51 - rG
 		}
-		c.G += rG
 	} else {
-		if c.G <= 51 {
+		if (c.G - rG) < 0 {
 			c.G = 0
+		} else {
+			c.G -= rG
 		}
-		c.G -= rG
 	}
 
 	rB := c.B % 51
-
-	if (rB > 51) || c.B < 51 {
-		if c.B >= 204 {
+	if rB > 25 {
+		if (c.B + 51 - rB) > 255 {
 			c.B = 255
+		} else {
+			c.B += 51 - rB
 		}
-		c.B += rB
-
 	} else {
-		if c.B <= 51 {
+		if (c.B - rB) < 0 {
 			c.B = 0
+		} else {
+			c.B -= rB
 		}
-		c.B -= rB
 	}
 
 }
