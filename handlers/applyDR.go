e65e68202970e53458ce1523bdeea01505304d58
Arthur <arthur001123@gmail.com>
2021-06-08 14:06:29 -0300

    add drawing and grayscale effect handlers

---
+++ handlers/applyDR.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"fmt"
+	"image/png"
+	"net/http"
+
+	"github.com/ArthurPaivaT/mergi/effects"
+	"github.com/ArthurPaivaT/mergi/utils"
+)
+
+func ApplyDR(w http.ResponseWriter, r *http.Request) {
+
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		err := fmt.Errorf("Missing file")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	img, err := utils.Decode(file)
+	if err != nil {
+		err := fmt.Errorf("Error Decoding: %w", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	drawing := effects.ToDrawing(img)
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	png.Encode(w, drawing)
+}
--- server/server.go
+++ server/server.go
@@ -15,6 +15,8 @@ func Start(resCh chan error) {
 	router := mux.NewRouter()
 
 	router.Handle("/applyBW", (http.HandlerFunc(handlers.ApplyBW))).Methods("POST")
+	router.Handle("/applyGS", (http.HandlerFunc(handlers.ApplyGS))).Methods("POST")
+	router.Handle("/applyDR", (http.HandlerFunc(handlers.ApplyDR))).Methods("POST")
 
 	router.Use(mux.CORSMethodMiddleware(router))
 	// needs to be non-empty
